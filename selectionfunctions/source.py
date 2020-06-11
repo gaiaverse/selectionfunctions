03696bf6681dcb299af0c526ca3653d40aafa617
andrew.everall1995@gmail.com <andrew.everall1995@gmail.com>
2020-06-11 18:28:21 +0100

    Added APOGEE selectionfunction code. Not yet tested.

--- selectionfunctions/source.py
+++ selectionfunctions/source.py
@@ -83,3 +83,55 @@ def ensure_gaia_g(f):
         return out
 
     return _wrapper_func
+
+
+def ensure_tmass_hjk(f):
+    """
+    A decorator for class methods of the form
+
+    .. code-block:: python
+
+        Class.method(self, coords, **kwargs)
+
+    where ``coords`` is an :obj:`astropy.coordinates.SkyCoord` object.
+
+    The decorator ensures that the ``coords`` that gets passed to
+    ``Class.method`` is a flat array of Equatorial coordinates. It also reshapes
+    the output of ``Class.method`` to have the same shape (possibly scalar) as
+    the input ``coords``. If the output of ``Class.method`` is a tuple or list
+    (instead of an array), each element in the output is reshaped instead.
+
+    Args:
+        f (class method): A function with the signature
+            ``(self, coords, **kwargs)``, where ``coords`` is a :obj:`SkyCoord`
+            object containing an array.
+
+    Returns:
+        A function that takes :obj:`SkyCoord` input with any shape (including
+        scalar).
+    """
+
+    @wraps(f)
+    def _wrapper_func(self, sources, **kwargs):
+        # t0 = time.time()
+
+        has_photometry = hasattr(sources, 'photometry')
+        if has_photometry:
+            has_tmass_h = 'tmass_h' in sources.photometry.measurement.keys()
+            has_tmass_jk = 'tmass_jk' in sources.photometry.measurement.keys()
+            if has_tmass_h: pass
+            else:
+                print('No 2MASS H magnitude passed (tmass_h), but transformation is not yet implemented.')
+                raise ValueError('You need to pass in 2MASS H photometric magnitudes to use this selection function.')
+            if has_tmass_jk: pass
+            else:
+                print('No 2MASS J-K colour passed (tmass_jk), but transformation is not yet implemented.')
+                raise ValueError('You need to pass in 2MASS J-K colour to use this selection function.')
+        else:
+            raise ValueError('You need to pass in 2MASS H photometric magnitudes and J-K colours to use this selection function.')
+
+        out = f(self, sources, **kwargs)
+
+        return out
+
+    return _wrapper_func
